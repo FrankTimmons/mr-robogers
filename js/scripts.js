9b6a8355e22ec17e6b05e0778400c9fcb4f43281
FrankTimmons <franktimmonspdx@gmail.com>
2022-06-10 11:38:20 -0700

    Move parseInt to ui logic for number input, add spaces before numbers in the array to make output cleaner to read.

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,8 @@ function beepBoop(number, name){
   if (Number(name)){
     return "Please input a string for name";
   }
-  parsedNum = parseInt(number);
   numArray = [];
-  for (let index = 0; index <= parsedNum; index +=1){
+  for (let index = 0; index <= number; index +=1){
     if (index.toString().indexOf('3') > -1){
       numArray.push(" Won't you be my neighbor " + name + "?");
     }else if (index.toString().indexOf('2') > -1){
@@ -15,7 +14,7 @@ function beepBoop(number, name){
     }else if (index.toString().indexOf('1') > -1){
       numArray.push(" Beep!");
     }else {
-      numArray.push(index);
+      numArray.push(" " + index);
     }
   }
   return numArray;
@@ -24,7 +23,7 @@ function beepBoop(number, name){
 $(document).ready(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
-    const numberPass = $("input#number").val();
+    const numberPass = parseInt($("input#number").val());
     const namePass = $("input#name").val();
     $(".arrayOutput").text(beepBoop(numberPass, namePass));
   });
